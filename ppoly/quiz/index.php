c04b11af59c176e2d9728004590086f892a793f0
VuVanHai <haivv279@gmail.com>
2023-12-13 15:10:35 +0900

    231213

--- ppoly/quiz/index.php
+++ ppoly/quiz/index.php
@@ -3,10 +3,9 @@ $conn = mysqli_connect("localhost", "root", "", "ppoly");
 mysqli_set_charset($conn,"utf8");
 
 
-$result = mysqli_query($conn, "SELECT summary, image, answer1,answer2, answer3, answer4, correct, createDate FROM quiz ORDER BY RAND() "); 
+$result = mysqli_query($conn, "SELECT question, image, answer1,answer2, answer3, answer4, correct, createDate FROM quiz ORDER BY RAND() "); 
 
 // Connect to MySQL database
-
 
 
 mysqli_set_charset($conn, 'utf8');
